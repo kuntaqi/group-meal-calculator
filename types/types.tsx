d2e1f26aac78910f6f6f2492ff59fbf30322f161
Taqi <ataqiyudin21@gmail.com>
2025-06-15 21:54:07 +0700

    update repo

--- types/types.tsx
+++ types/types.tsx
@@ -1,33 +1,33 @@
 export interface Item {
-    name: string,
-    price: number,
-    quantity: number,
-    total: number
+	name: string,
+	price: number,
+	quantity: number,
+	total: number
 }
 
 export interface User {
-    name: string,
-    items: Item[]
+	name: string,
+	items: Item[]
 }
 
 export interface Discount {
-    type: "percentage" | "amount",
-    value: number
+	type: "percentage" | "amount",
+	value: number
 }
 
 export interface Result {
-    name: string,
-    items: Item[],
-    subtotal: number,
-    discount: number,
-    shipping: number,
-    share: number
+	name: string,
+	items: Item[],
+	subtotal: number,
+	discount: number,
+	shipping: number,
+	share: number
 }
 
 export interface ReceiptProps {
-    billPayer: string
-    date: string
-    restaurantName: string
-    results: Result[]
-    currency: string
+	billPayer: string
+	date: string
+	restaurantName: string
+	results: Result[]
+	currency: string
 }
